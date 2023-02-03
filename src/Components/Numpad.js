ce4ebd3319c7a89c94dd69cf1c9bcb5c3a1c3d8d
stilltrillogf <2004kowalewicz@gmail.com>
2023-02-03 09:36:30 +0100

    Numpad layout

--- src/Components/Numpad.js
+++ src/Components/Numpad.js
@@ -1,3 +1,32 @@
+function Number({ value }) {
+  return (
+    // TODO: add dynamic class with value
+    <div className={`Numpad__Number Numpad__Number__${value}`}>{value}</div>
+  );
+}
+
+function Zero() {
+  return <div className="Numpad__Zero">0</div>;
+}
+
+function Decimal() {
+  return <div className="Numpad__Decimal">.</div>;
+}
+
 export default function Numpad() {
-  return <div className="Numpad">Numpad</div>;
+  return (
+    <div className="Numpad">
+      <Number value={7} />
+      <Number value={8} />
+      <Number value={9} />
+      <Number value={4} />
+      <Number value={5} />
+      <Number value={6} />
+      <Number value={1} />
+      <Number value={2} />
+      <Number value={3} />
+      <Zero />
+      <Decimal />
+    </div>
+  );
 }
