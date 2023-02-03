dc3f65c78a2a3238748679b5523dc60e6736013d
stilltrillogf <2004kowalewicz@gmail.com>
2023-02-03 11:44:02 +0100

    Display layout

--- src/Components/Display.js
+++ src/Components/Display.js
@@ -1,3 +1,8 @@
 export default function Display() {
-  return <div className="Display">Display</div>;
+  return (
+    <div className="Display">
+      <div className="Display__Operation">1234</div>
+      <div className="Display__CurrentNumbers">5678</div>
+    </div>
+  );
 }
